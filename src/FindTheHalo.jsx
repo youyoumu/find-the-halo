556033706de4007d24e3626fb54b3fb50568cc4b
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-05-04 13:41:46 +0700

    Place mark on clicked coord

--- src/FindTheHalo.jsx
+++ src/FindTheHalo.jsx
@@ -1,8 +1,9 @@
 import image0 from './images/0.png'
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 
 export default function FindTheHalo() {
   const imageContainer = useRef(null)
+  const [mark, setMark] = useState(null)
   const handleClick = (e) => {
     const containerX = imageContainer.current.getBoundingClientRect().x
     const containerY = imageContainer.current.getBoundingClientRect().y
@@ -13,13 +14,24 @@ export default function FindTheHalo() {
     const x = ((e.pageX - containerX) / containerWidth) * 100
     const y = ((e.pageY - containerY) / containerHeight) * 100
 
+    const markX = e.pageX - containerX - 8
+    const markY = e.pageY - containerY - 8
+
+    setMark(
+      <div
+        className={`absolute w-4 h-4 bg-warning rounded-full`}
+        style={{ left: `${markX}px`, top: `${markY}px` }}
+      ></div>
+    )
+
     console.log(x, y)
   }
 
   return (
     <div>
-      <div className="mt-8" ref={imageContainer} onClick={handleClick}>
+      <div className="static" ref={imageContainer} onClick={handleClick}>
         <img src={image0} />
+        {mark}
       </div>
     </div>
   )
